fb1f7746ddddfeb6ef8675046d3eb5190f837fe0
chenli <chenl@enn.com>
2019-04-13 18:53:01 +0800

    新增 列表选择页面

---
+++ app/src/main/java/cn/ygyg/yjf/modular/internet/entity/SelectModel.kt
@@ -0,0 +1,9 @@
+package cn.ygyg.yjf.modular.internet.entity
+
+import java.io.Serializable
+
+class SelectModel : Serializable {
+
+    var showName: String? = null
+    var isMultipleCheck: Boolean = false
+}
