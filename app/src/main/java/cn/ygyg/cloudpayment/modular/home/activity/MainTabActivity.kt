5f7d52a1044b6884b3273bd0bb45df5909e2bbd3
gaohuilong <jadend@163.com>
2019-10-30 15:19:59 +0800

    云缴费

--- app/src/main/java/cn/ygyg/cloudpayment/modular/home/activity/MainTabActivity.kt
+++ app/src/main/java/cn/ygyg/cloudpayment/modular/home/activity/MainTabActivity.kt
@@ -13,8 +13,10 @@ class MainTabActivity : BaseIndicatorActivity() {
    override fun  getViewPagerId(): Int = R.id.view_pager
 
     override fun initTabsInfo(tabs: ArrayList<TabInfo>) {
-        tabs.add(TabInfo(1, ResourceUtil.getString(getViewContext(), R.string.tab_home), R.mipmap.tab_home_normal, R.mipmap.tab_home_press, HomeFragment::class.java))
-        tabs.add(TabInfo(2, ResourceUtil.getString(getViewContext(), R.string.tab_my), R.mipmap.tab_my_normal, R.mipmap.tab_my_press, MyFragment::class.java))
+        tabs.add(TabInfo(1, ResourceUtil.getString(getViewContext(), R.string.tab_home),
+                R.mipmap.tab_home_normal, R.mipmap.tab_home_press, HomeFragment::class.java))
+        tabs.add(TabInfo(2, ResourceUtil.getString(getViewContext(), R.string.tab_my),
+                R.mipmap.tab_my_normal, R.mipmap.tab_my_press, MyFragment::class.java))
     }
 
     override fun getTagIndicatorViewId(): Int = R.id.layout_tab
